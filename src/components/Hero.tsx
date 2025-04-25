64664d40fad2f2b349d3454844a54ff853ba52e3
21N81A66K0) <udaykiran.2688@gmail.com>
2025-04-25 14:45:13 +0530

    Fixed href in Hero component and removed unintended command

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
             </p>
             <div className="flex gap-4 justify-center">
               <motion.a 
-                href="https://drive.google.com/file/d/10McxjLhS1j6F46Q4ic0KRj5oeQt6bpgX/view?usp=drivesdkk"
+                href="https://drive.google.com/file/d/1c8ugsOXmOvI5-2kdkogz7b5xHbSyWUmc/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-primary flex items-center gap-2"
